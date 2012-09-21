7e9b791c366a5014786078f32d6f632be6a6023c
Tony Liu <fentensoft@163.com>
2012-09-21 23:00:16 +0800

    Initial commit

---
+++ repack-bootimg.pl
@@ -0,0 +1,22 @@
+#!/usr/bin/perl -W
+
+use strict;
+use Cwd;
+
+my $dir = getcwd;
+
+my $usage = "repack-bootimg.pl <compress mode: gzip/xz/lzma> <kernel> <ramdisk-directory> <outfile>\n";
+
+die $usage unless $ARGV[0] && $ARGV[1] && $ARGV[2] && $ARGV[3];
+
+chdir $ARGV[2] or die "$ARGV[2] $!";
+
+system ("find . | cpio -o -H newc | $ARGV[0] > $dir/ramdisk-repack.cpio.gz");
+
+chdir $dir or die "$ARGV[2] $!";;
+
+system ("./mkbootimg --kernel $ARGV[1] --ramdisk ramdisk-repack.cpio.gz -o $ARGV[3]");
+
+unlink("ramdisk-repack.cpio.gz") or die $!;
+
+print "\nrepacked boot image written at $ARGV[3]\n";
